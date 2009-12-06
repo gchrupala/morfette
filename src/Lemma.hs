ae2d36660802b0cbdb2b84365ae53cde01a24844
pitekus <pitekus@a7a69d2c-e1b7-11de-9e61-43d41527e390>
2009-12-06 16:03:58 +0000

    Modified featureset to use lexicon features.

--- src/Lemma.hs
+++ src/Lemma.hs
@@ -1,6 +1,6 @@
 module Lemma (featureSpec,apply,make)
 where
-import qualified Data.Map as M
+import qualified Data.Map as Map
 import GramLab.Morfette.Features.Common
 import qualified GramLab.Data.Diff.EditTree as E
 import Debug.Trace
@@ -26,12 +26,14 @@ prepare = lowercase
 
 theFeatures  global tic = focusFeatures     (focus tic)
     where focusFeatures (Just (Str form:Str label:_)) = [ Sym $ low form, Sym $ label
-                                                , Sym $ spellingSpec form
+                                                        , Sym $ spellingSpec form
+                                                        , lexmap (low form)
                                                 ]
                                               ++ prefixes maxPrefix (low form)
                                               ++ suffixes maxSuffix (low form)
           focusFeatures other = error $ "Lemma.theFeatures: " ++ show other
           low = lowercase
+          lexmap w = Set $ map (\(l,p,c) -> show $ make w l) $ Map.findWithDefault [] w (dictLex global)
 
 decode str = case reads str of
                [(s,"")] -> s
