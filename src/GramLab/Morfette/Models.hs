55de017a874a41af1680e7476a98637be11bc325
pitekus <pitekus@a7a69d2c-e1b7-11de-9e61-43d41527e390>
2010-01-20 10:13:57 +0000

    Reformatted

--- src/GramLab/Morfette/Models.hs
+++ src/GramLab/Morfette/Models.hs
@@ -50,9 +50,11 @@ beamSearch n cfs pzs =
     else -- otherwise apply each classifier in turn in the lzipper seq,
          -- prune, adjust probs
          let f pzs' model =   prune n 
-                              $ flip concatMap pzs'
-                                    $ \(z,p) -> flip map (model $ z) 
-                                                $ \(c,c_p) -> (modify (\x -> x ++ [c]) z,p*c_p)
+                            . flip concatMap pzs'
+                            $ \(z,p) -> 
+                                flip map (model $ z) 
+                                  $ \(c,c_p) -> 
+                                      (modify (\x -> x ++ [c]) z,p*c_p)
          in beamSearch n cfs . map (\(z,p) -> (slide z,p)) $! (foldl f pzs cfs)
 
 -- pruning and prepruning
@@ -65,26 +67,34 @@ collectUntil cond f z (x:xs) = let z' = (f $! x) $! z
                                    else x: collectUntil cond f z' xs
 mkPreprune th = collectUntil (\x z -> th > snd x / z) ((+) . snd) 0
 
-data FeatureSpec a = FS { label    :: Tok a -> Smth a
-                        , features :: LZipper (Tok a) (Tok a) (Tok a) -> [Feature String Double] 
-                        , preprune :: ProbDist (Smth a) -> ProbDist (Smth a)
-                        , check    :: LZipper (Tok a) (Tok a) (Tok a) -> Smth a -> Bool 
-                        , trainSettings :: M.TrainSettings }
+data FeatureSpec a = 
+    FS { label    :: Tok a -> Smth a
+       , features :: LZipper (Tok a) (Tok a) (Tok a) -> [Feature String Double] 
+       , preprune :: ProbDist (Smth a) -> ProbDist (Smth a)
+       , check    :: LZipper (Tok a) (Tok a) (Tok a) -> Smth a -> Bool 
+       , trainSettings :: M.TrainSettings }
 
 trainFun ::  (Ord a,Show a) => [FeatureSpec a] -> [[Tok a]]  -> [Model a]
 trainFun fspecs sents = 
   let ms = train fspecs sents
   in (zipWith toModelFun fspecs ms) 
  
-train :: (Ord a) => [FeatureSpec a] -> [[Tok a]]  -> [M.Model (Label a) Int String Double]
+train :: (Ord a) => 
+         [FeatureSpec a] 
+      -> [[Tok a]]  
+      -> [M.Model (Label a) Int String Double]
 train fspecs sents = 
   flip map fspecs
-           $ \fs ->  M.train (trainSettings fs) (concatMap (sentToExamples fs) sents)
+           $ \fs ->  M.train (trainSettings fs) 
+                     . concatMap (sentToExamples fs) $ sents
   
  
 
 
-toModelFun :: (Ord a,Show a) => FeatureSpec a -> (M.Model (Label a) Int String Double) -> Model a
+toModelFun :: (Ord a,Show a) => 
+              FeatureSpec a 
+           -> (M.Model (Label a) Int String Double) 
+           -> Model a
 toModelFun fs m = 
     \ z -> case preprune fs 
                 .  (\xs -> case filter (check fs z . fst) xs of
@@ -96,17 +106,25 @@ toModelFun fs m =
 
 predict :: Int -> [Model a] -> [[Tok a]] -> [[Tok a]]
 predict beamSize models sents = map predictOne sents
-    where predictOne s = fst . head . beamSearch beamSize models $ [(fromList s,1)]
+    where predictOne s = fst 
+                         . head 
+                         . beamSearch beamSize models 
+                         $ [(fromList s,1)]
 
 predictPipeline :: Int -> [Model a] -> [[Tok a]] -> [[Tok a]]
 predictPipeline beamSize models sents = map predictOne sents
-    where predictOne s = foldl (\s1 m -> fst . head . beamSearch beamSize [m] $ [(fromList s1,1)]) s models
+    where predictOne s = foldl (\s1 m -> fst . head . beamSearch beamSize [m] 
+                                         $ [(fromList s1,1)]) s models
                                
 
 type Label a = Smth a
-sentToExamples ::  FeatureSpec a -> [Tok a] -> [(Label a,[Feature String Double])]
+sentToExamples ::  FeatureSpec a 
+               -> [Tok a] 
+               -> [(Label a,[Feature String Double])]
 sentToExamples fs xs = slideThru f (fromList xs)
-    where f z = (label fs (fromMaybe (error "sentToExample:fromMaybe") (focus z)), features fs z)
+    where f z = (label fs (fromMaybe (error "sentToExample:fromMaybe") 
+                                     (focus z))
+                , features fs z)
     
 slideThru f z  | atEnd z   = []
 slideThru f z              = f z:slideThru f (slide z) 
