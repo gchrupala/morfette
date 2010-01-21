ab3b563b5f4075b74a8bf1eb5c2895f48f98f4d3
pitekus <pitekus@a7a69d2c-e1b7-11de-9e61-43d41527e390>
2010-01-21 18:44:04 +0000

    Remove unnecessary files. Include Other-modules field in .cabal file.

--- trunk/src/GramLab/Morfette/BinaryInstances.hs
+++ trunk/src/GramLab/Morfette/BinaryInstances.hs
@@ -2,8 +2,6 @@ module GramLab.Morfette.BinaryInstances
 where
 import GramLab.Data.Diff.EditListRev
 import GramLab.Data.Diff.EditTree 
-import GramLab.Data.Diff.EditListBidi 
-import qualified GramLab.Data.Diff.EditTree2Rev as ET2R
 import Data.Binary
 import Control.Monad (liftM,liftM2,liftM3,liftM4)
 instance Binary a => Binary (Edit a) where
@@ -17,7 +15,8 @@ instance Binary a => Binary (Edit a) where
 
 instance Binary s => Binary (EditTree s a) where
     put (Replace xs ys)  = put (0::Word8) >> put xs >> put ys
-    put (Split i j lt rt) = put (1::Word8) >> put i >> put j >> put lt >> put rt
+    put (Split i j lt rt) = put (1::Word8) >> put i >> put j 
+                            >> put lt >> put rt
     get = do
       tag <- get
       case tag::Word8 of
@@ -27,16 +26,3 @@ instance Binary s => Binary (EditTree s a) where
 instance Binary a => Binary (EditListRev a) where
     put (ELR x) = put x
     get = liftM ELR get
-{- FIXME the duplicate definition 
-    Duplicate instance declarations:
-      instance (Binary s) => Binary (ET2R.EditTree s a)
-	-- Defined at /home/gchrupala/work/gramlab/morfette/src/GramLab/Morfette/BinaryInstances.hs:(30,0)-(32,27)
-      instance (Binary s) => Binary (ET2R.EditTree s a)
-	-- Defined in GramLab.Data.Diff.FuzzyEditTree
-instance Binary s => Binary (ET2R.EditTree s a) where
-    put (ET2R.ETR x) = put x
-    get = liftM ET2R.ETR get
--}
-instance Binary a => Binary (EditListBidi a) where
-    put (ELB x y) = put x >> put y
-    get = liftM2 ELB get get
