ace54e4e213d2e5fd96054f965df241ae8a7936c
pitekus <pitekus@a7a69d2c-e1b7-11de-9e61-43d41527e390>
2010-01-25 15:05:31 +0000

    Implemented extract-features command in GramLab.Morfette.Utils. Changed lexicon parsing and filtering code (made filtering optional). Updates to morfette.cabal

--- src/GramLab/Morfette/Lang/Conf.hs
+++ src/GramLab/Morfette/Lang/Conf.hs
@@ -17,6 +17,8 @@ import qualified Data.ByteString.Lazy as BS
 import Data.Maybe (catMaybes)
 import GramLab.Utils (padRight,splitWith,splitInto,lowercase)
 import GramLab.Morfette.Token
+import Debug.Trace
+import qualified Control.Monad.State as S
     
 type Lang = String
 type Lexicon = Map.Map String [(String, String)]
@@ -44,21 +46,25 @@ readConf path = do
   txt <- BS.readFile path
   return (Binary.decode txt)
 
-parseLexicon :: Set.Set String -> String -> Lexicon 
-parseLexicon toks = Map.fromList 
-                         . filter (\(w,_) -> w `Set.member` toks)
-                         . map parseEntry 
-                         . lines 
-                 
-    
-parseEntry :: String -> (String, [(String, String)])
+parseLexicon :: Maybe (Set.Set String) -> String -> Lexicon 
+parseLexicon toks = 
+    Map.fromListWith (++)
+           . flip S.evalState Map.empty 
+           . mapM (\(f,(l,p)) -> do
+                     f' <- atomize f
+                     l' <- atomize l
+                     p' <- atomize p
+                     return (f',[(l',p')]))
+           . maybe id (\d -> filter (\(w,_) -> w `Set.member`d)) toks
+           . concatMap parseEntry 
+           . lines 
+
+parseEntry :: String -> [(String,(String, String))]
 parseEntry line = 
     let (form:pairs) = words line 
-        len = fromIntegral $ length pairs
-        val = map (\ [lemma,pos] -> (lowercase lemma,lowercase pos)) 
-                       . splitInto 2 
-                       $ pairs
-    in  (val == val `seq` form,val)
+    in [ lemma == lemma && pos == pos `seq` (form,(lemma,lowercase pos))
+             | [lemma,pos] <- splitInto 2 $ pairs ]
+
 
 
 splitPOS :: Lang -> String -> [String]
@@ -67,3 +73,10 @@ splitPOS "pl" = splitWith (==':')
 splitPOS "cy" = splitWith (=='-')
 splitPOS "ga" = splitWith (=='-')
 splitPOS _    = map return
+
+atomize str = do
+  d <- S.get
+  case Map.lookup str d of
+    Nothing -> do S.put (Map.insert str str d)
+                  return str
+    Just str' -> return str'
